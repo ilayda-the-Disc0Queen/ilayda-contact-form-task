90cf3608df8ed7e33dc298dd7e6c442b5521bbbb
Ilayda <ilayda.bakare@gmail.com>
2023-11-06 20:58:45 +0000

    avoid submitting same messge twice and fixed dimensions for draggable textarea

--- src/js/showMessage.js
+++ src/js/showMessage.js
@@ -1,49 +1,48 @@
 let messageContent = {
-	"success": "Your form has been successfully submitted!",
-	"error": "There was a validation error. Please check your input!",
-	"alreadySubmitted": "You have already submitted this form!",
+  success: "Your form has been successfully submitted!",
+  error: "There was a validation error. Please check your input!",
+  alreadySubmitted: "You have already submitted this form!",
 };
 
 let successState = false;
 
 // Getter function for successState
 export function getHasSubmitted() {
-    return successState;
+  return successState;
 }
 
 // Setter function for successState
 export function setHasSubmitted(value) {
-    successState = value;
+  successState = value;
 }
 
 export function showSuccessMessage(didSucceed) {
-    const modal = document.getElementById('modal');
-    const modalMessage = document.getElementById('modal-message');
-    const closeModal = document.querySelector('.close-btn');
+  const modal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+  const closeModal = document.querySelector(".close-btn");
 
-    if (didSucceed) {
-		// TODO: The `alreadySubmitted` message is wrongly showing before 
-		// the user has successfully submitted the form. We need to fix this!
-
-        if (getHasSubmitted()) {
-            modalMessage.textContent = messageContent.alreadySubmitted;
-        } else {
-            modalMessage.textContent = messageContent.success;
-            setHasSubmitted(true);
-        }
+  if (didSucceed) {
+    // TODO: The `alreadySubmitted` message is wrongly showing before
+    // the user has successfully submitted the form. We need to fix this!
+    if (getHasSubmitted()) {
+      modalMessage.textContent = messageContent.alreadySubmitted;
     } else {
-        modalMessage.textContent = messageContent.error;
+      modalMessage.textContent = messageContent.success;
+      setHasSubmitted(true);
     }
+  } else {
+    modalMessage.textContent = messageContent.error;
+  }
 
-    modal.style.display = "block";
+  modal.style.display = "block";
 
-    closeModal.onclick = function() {
-        modal.style.display = "none";
+  closeModal.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
     }
-
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    }
+  };
 }
--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -1,20 +1,35 @@
-import { showSuccessMessage, setHasSubmitted } from './showMessage.js';
+import { showSuccessMessage } from "./showMessage.js";
+
+const messagesBySender = {};
 
 export function validate() {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const email = document.querySelector('input[name="email"]').value;
-        const message = document.querySelector('textarea[name="message"]').value;
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const email = document.querySelector('input[name="email"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-        // Basic form validation: check for '@' in email field and non-blank message field
-        // TODO: This validation could probably be improved!
-        if (emailRegex.test(email) && message) {
-            showSuccessMessage(true);
-        } else {
-            showSuccessMessage(false);
+    function addMessage(senderEmail, message) {
+      if (senderEmail in messagesBySender) {
+        // before adding new message, check it doesn't already exist from this user
+        // - we don't want duplicates!
+        if (!messagesBySender[senderEmail].includes(message)) {
+          messagesBySender[senderEmail].push(message);
+          // given more time I'd like to display a different message to the user than alreadySubmitted
         }
-    });
+      } else {
+        messagesBySender[senderEmail] = [message];
+      }
+    }
+
+    // TODO: This validation could probably be improved!
+    if (emailRegex.test(email) && message) {
+      addMessage(email, message.trim());
+      showSuccessMessage(true);
+    } else {
+      showSuccessMessage(false);
+    }
+  });
 }
